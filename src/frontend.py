2fbe13a14e6370f50f5a24136a2516b180a7dbd8
lihaoz-barry <lihaoz0214@gmail.com>
2025-12-09 21:24:24 -0600

    Revert "Fix backend startup: Change default port from 5000 to 8888"
    This reverts commit 17683bd1b047dc1a5c91468f509d369bf2695caf.

--- src/frontend.py
+++ src/frontend.py
@@ -9,10 +9,7 @@ import os
 import shutil
 import json
 
-# Backend URL - configurable via environment variable
-# Default port is 8888 to avoid Windows reserved port ranges
-BACKEND_PORT = os.environ.get('BACKEND_PORT', '8888')
-BACKEND_URL = f"http://127.0.0.1:{BACKEND_PORT}"
+BACKEND_URL = "http://127.0.0.1:5000"
 URLS_FILE = "urls.json"
 
 class CometRunnerApp:
